abf25cf75a94f1322ca1ba0799426e43eb820fbf
Tibor Leupold <tibor@lpld.io>
2019-11-09 13:00:27 -1000

    Moved requirements into respective file

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import find_packages, setup
 
 
+def get_requirements():
+    with open("requirements.txt") as reqf:
+        return [line.strip() for line in reqf.readlines()]
+
+
 setup(
     name="log100days",
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        "flask",
-        "markdown2",
-        "requests",
-        "python-dotenv"
-    ]
+    install_requires=get_requirements(),
 )
