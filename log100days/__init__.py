2e2c6b86eb23e1152017b35faac7bbf9f77434ff
Tibor Leupold <tibor@lpld.io>
2020-03-22 16:18:00 -0700

    Make .quartenv file work for development
    This is a feature that is included in Flask, but not mirrored in Quart.
    This required a simple step of loading the file with python-dotenv
    while the app is loaded.

--- log100days/__init__.py
+++ log100days/__init__.py
@@ -1,8 +1,13 @@
 from quart import Quart
+from dotenv import load_dotenv, find_dotenv
+
+# To make .quartenv file work to set the Quart related environment variables.
+# This is only for development purposes.
+load_dotenv(find_dotenv(".quartenv"))
 
 app = Quart(__name__, instance_relative_config=True)
 app.config.from_mapping(
-    SECRET_KEY="dev"
+    SECRET_KEY="dev",
 )
 app.config.from_pyfile("config.py", silent=True)
 
